b2653861ca15a698a014a0fb1b8d1def657ef64e
robinxbaker <114107792+robinxbaker@users.noreply.github.com>
2025-09-14 18:20:58 -0500

    Add age checking functionality in CheckingTheAge.java

---
+++ 9_14_25/CheckingTheAge.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class CheckingTheAge {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("How old are you?");
+        
+        int age = Integer.valueOf(scan.nextLine());
+        
+        System.out.println((age >= 0 && age <= 120) ? "OK" : "Impossible!");
+    }
+}
