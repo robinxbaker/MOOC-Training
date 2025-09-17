ec0d6d7d7d82af52916e72b2329985d6f36cc494
robinxbaker <114107792+robinxbaker@users.noreply.github.com>
2025-09-17 09:33:21 -0500

    Implement method to find the greatest of three numbers

---
+++ 9_17_25/Greatest.java
@@ -0,0 +1,22 @@
+
+public class Greatest {
+
+    public static int greatest(int number1, int number2, int number3) {
+        
+        if (number1 >= number2){
+            if (number1 >= number3){
+                return number1;
+            }
+            return number3;
+        }
+        if (number3 >= number2){
+            return number3;
+        }
+        return number2;
+    }
+
+    public static void main(String[] args) {
+        int result = greatest(2, 7, 3);
+        System.out.println("Greatest: " + result);
+    }
+}
