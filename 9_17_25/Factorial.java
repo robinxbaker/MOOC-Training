2f291864dde6203e4425db7a5c711da108d1852b
robinxbaker <114107792+robinxbaker@users.noreply.github.com>
2025-09-17 06:41:33 -0500

    Create Factorial.java

---
+++ 9_17_25/Factorial.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        
+        int number = Integer.valueOf(scanner.nextLine());
+        int factorial = 1;
+        
+        if (number == 0){
+            System.out.println("Factorial: 1");
+        }
+        for (int i = 1; i <= number; i++){
+            factorial *= i;
+        }
+        System.out.println("Factorial: " + factorial);
+    }
+}
